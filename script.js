2ed460cc01b12d051f4a3b0e085132844fc3b1f3
Suchithra2505 <147540982+Suchithra2505@users.noreply.github.com>
2023-12-13 14:41:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function firstWord(s) {
   // your code here
+	if(!s){
+		return '';
+	}
+	const initialWord = s.split(' ');
+	let fW = initialWord[0];
+	return fW;
 }
 
 // Do not change the code below
