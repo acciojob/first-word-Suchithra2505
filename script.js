a9321c53f41339d89a55937cb6ecbf213525e272
Suchithra2505 <147540982+Suchithra2505@users.noreply.github.com>
2023-12-28 16:09:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 function firstWord(s) {
   // your code here
-	if(!s){
-		return '';
-	}
-	const initialWord = s.split(' ');
-	let fW = initialWord[0];
-	return fW;
+	if (s === '' || !s.includes(' ')) {
+    return s;
+  }
+
+  // Extract the first word up to the first space
+  const firstSpaceIndex = s.indexOf(' ');
+  const result = s.substring(0, firstSpaceIndex);
+
+	return result;
 }
 
 // Do not change the code below
